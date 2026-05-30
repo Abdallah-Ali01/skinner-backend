0a727ccafa51dd544e3b47b0de240c91ab5403d2
Abdallah Ali <abdallah.ali.abdallah019@gmail.com>
2026-05-31 00:22:15 +0300

    fix: allow cross-origin image loading by setting helmet crossOriginResourcePolicy

--- src/app.js
+++ src/app.js
@@ -39,6 +39,7 @@ app.use(cors({
 }));
 app.use(helmet({
   contentSecurityPolicy: false,
+  crossOriginResourcePolicy: { policy: "cross-origin" },
 }));
 app.use(morgan("dev"));
 app.use(express.json());
